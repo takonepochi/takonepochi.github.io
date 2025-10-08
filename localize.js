723585dd3ff88f1e3363a9cd48e6cd21570b38c8
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2025-10-08 18:54:52 -0400

    Delete localize.js

--- localize.js
+++
@@ -1,8 +0,0 @@
- // Check if URL contains ?jp
-    const isJP = window.location.search.includes('jp');
-
-    if (isJP) {
-      document.querySelectorAll('[data-en]').forEach(el => {
-        el.innerHTML = el.getAttribute('data-jp');
-      });
-    }
